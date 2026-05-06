d68810e9379bfc6ae844672e0986451d7e827033
bouaidzakariaa-arch <bouaidzakariaa@gmail.com>
2026-05-06 16:07:20 +0100

    feat: chatbot smart devis collection

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -249,7 +249,7 @@ export default function Navbar() {
               <Link href="/" className="flex items-center gap-3 flex-shrink-0 group" onClick={fermer}>
                 <div
                   className="relative transition-all duration-300 overflow-hidden rounded-xl group-hover:scale-105"
-                  style={{ width: scrolled ? 48 : 60, height: scrolled ? 48 : 60 }}>
+                  style={{ width: scrolled ? 56 : 72, height: scrolled ? 56 : 72 }}>
                   <Image
                     src="/images/logo.png"
                     alt="CADOZAT"
